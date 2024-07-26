fa60d1ecf40a91262cdeb659735a5148c3395ccc
Liu Ziming <liuziming.lzm@alibaba-inc.com>
2024-07-26 17:39:20 +0800

    Feat/test framework (#10)
    * feat: init semconv and mongo plugin
    
    * fix: upgrade go version to 1.21
    
    * rm arms logic
    
    * test: test framework support
    
    * set max test go version to go1.21.12
    
    * join pwd
    
    * add more log
    
    * change log level

---
+++ pkg/verifier/util_test.go
@@ -0,0 +1,14 @@
+package verifier
+
+import "testing"
+
+func TestGetFreePort(t *testing.T) {
+	port, err := GetFreePort()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if port == 0 {
+		t.Fatal("port is 0")
+	}
+	t.Logf("port is %d", port)
+}
