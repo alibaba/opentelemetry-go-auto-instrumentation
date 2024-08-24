7d11331224ee3f7b0cdff813565b583eb7feccd8
JellyTony <106534114+JellyTony@users.noreply.github.com>
2024-08-24 13:42:06 +0800

    feat(rule): support echo v4 (#64)
    * feat(rule): support echo v4
    
    * test(test): 增加单元测试代码
    
    * docs(docs): update readme.md
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * docs(docs): 更新插件顺序

---
+++ test/echo/v4.0.0/test_echo_basic.go
@@ -0,0 +1,59 @@
+// Copyright (c) 2024 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/alibaba/opentelemetry-go-auto-instrumentation/pkg/verifier"
+	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
+)
+
+func setup() {
+	engine := echo.New()
+	engine.Use(middleware.Logger())
+	engine.GET("/test", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"code": 1,
+			"msg":  c.Path(),
+		})
+	})
+
+	// Start server
+	engine.Logger.Fatal(engine.Start(":8080"))
+}
+
+func main() {
+	go setup()
+	time.Sleep(5 * time.Second)
+	client := &http.Client{}
+	resp, err := client.Get("http://127.0.0.1:8080/test")
+	defer resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	verifier.WaitAndAssertTraces(func(stubs []tracetest.SpanStubs) {
+		verifier.VerifyHttpClientAttributes(stubs[0][0], "GET", "GET", "http://127.0.0.1:8080/test", "http", "1.1", "tcp", "ipv4", "", "127.0.0.1:8080", 200, 0, int64(8080))
+		verifier.VerifyHttpServerAttributes(stubs[0][1], "GET /test", "GET", "http", "tcp", "ipv4", "", "127.0.0.1:8080", "Go-http-client/1.1", "", "/test", "", "/test", 200)
+		verifier.VerifyHttpServerAttributes(stubs[0][2], "GET /test", "GET", "http", "tcp", "ipv4", "", "127.0.0.1:8080", "Go-http-client/1.1", "", "/test", "", "/test", 200)
+	}, 1)
+}
