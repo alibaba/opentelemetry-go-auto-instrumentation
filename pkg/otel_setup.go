d53a7d0455f6113f3a8c0bc0dc5ee799f350a031
JellyTony <bytedev@qq.com>
2024-08-24 12:51:51 +0800

    feat(exporter): 增加 标准输出、zipkin、文件等自定义导出

--- pkg/otel_setup.go
+++ pkg/otel_setup.go
@@ -17,19 +17,23 @@ package pkg
 
 import (
 	"context"
-	"go.opentelemetry.io/otel/sdk/trace"
+	"fmt"
 	"log"
 	"os"
 	"strings"
 	"time"
 
+	"go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
+
 	"github.com/alibaba/opentelemetry-go-auto-instrumentation/pkg/verifier"
 	"go.opentelemetry.io/otel"
 	_ "go.opentelemetry.io/otel"
 	_ "go.opentelemetry.io/otel/baggage"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/exporters/zipkin"
 	"go.opentelemetry.io/otel/propagation"
 	_ "go.opentelemetry.io/otel/sdk/trace"
 )
@@ -41,6 +45,15 @@ import (
 const exec_name = "otelbuild"
 const report_protocol = "OTEL_EXPORTER_OTLP_PROTOCOL"
 const trace_report_protocol = "OTEL_EXPORTER_OTLP_TRACES_PROTOCOL"
+
+const (
+	KindNoop     = "noop"
+	KindFile     = "file"
+	KindStdout   = "stdout"
+	KindZipkin   = "zipkin"
+	KindOtlpGrpc = "grpc"
+	KindOtlpHttp = "http"
+)
 
 func init() {
 	path, err := os.Executable()
@@ -54,37 +67,68 @@ func init() {
 	initOpenTelemetry()
 }
 
-func newSpanProcessor(ctx context.Context) trace.SpanProcessor {
+func createSpanProcessor(ctx context.Context, endpoint, batcher string) (trace.SpanProcessor, error) {
 	if verifier.IsInTest() {
 		traceExporter := verifier.GetSpanExporter()
 		// in test, we just send the span immediately
 		simpleProcessor := trace.NewSimpleSpanProcessor(traceExporter)
-		return simpleProcessor
-	} else {
-		var traceExporter trace.SpanExporter
-		var err error
-		if os.Getenv(report_protocol) == "grpc" || os.Getenv(trace_report_protocol) == "grpc" {
-			traceExporter, err = otlptrace.New(ctx, otlptracegrpc.NewClient())
-			if err != nil {
-				log.Fatalf("%s: %v", "Failed to create the OpenTelemetry trace exporter", err)
-			}
-		} else {
-			traceExporter, err = otlptrace.New(ctx, otlptracehttp.NewClient())
-			if err != nil {
-				log.Fatalf("%s: %v", "Failed to create the OpenTelemetry trace exporter", err)
-			}
-		}
-		batchSpanProcessor := trace.NewBatchSpanProcessor(traceExporter)
-		return batchSpanProcessor
+		return simpleProcessor, nil
 	}
+
+	var (
+		err           error
+		traceExporter trace.SpanExporter
+	)
+	// Just support jaeger and zipkin now, more for later
+	switch batcher {
+	case KindZipkin:
+		traceExporter, err = zipkin.New(endpoint)
+	case KindOtlpGrpc:
+		// Always treat trace exporter as optional component, so we use nonblock here,
+		// otherwise this would slow down app start up even set a dial timeout here when
+		// endpoint can not reach.
+		// If the connection not dial success, the global otel ErrorHandler will catch error
+		// when reporting data like other exporters.
+		opts := []otlptracegrpc.Option{
+			otlptracegrpc.WithInsecure(),
+			otlptracegrpc.WithEndpoint(endpoint),
+		}
+		traceExporter, err = otlptracegrpc.New(ctx, opts...)
+	case KindStdout:
+		return stdouttrace.New()
+	case KindFile:
+		f, err := os.OpenFile(endpoint, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			return nil, fmt.Errorf("file exporter endpoint error: %s", err.Error())
+		}
+		traceExporter, err = stdouttrace.New(stdouttrace.WithWriter(f))
+	case KindNoop:
+		traceExporter, err = tracetest.NewNoopExporter(), nil
+	default:
+		// Not support flexible configuration now.
+		opts := []otlptracehttp.Option{
+			otlptracehttp.WithInsecure(),
+			otlptracehttp.WithEndpoint(endpoint),
+		}
+		traceExporter, err = otlptracehttp.New(ctx, opts...)
+	}
+
+	return trace.NewBatchSpanProcessor(traceExporter), err
 }
 
 func initOpenTelemetry() func() {
 	ctx := context.Background()
 
-	var batchSpanProcessor trace.SpanProcessor
+	endpoint := os.Getenv("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT")
+	reportProtocol := os.Getenv(report_protocol)
+	if reportProtocol == "" {
+		reportProtocol = os.Getenv(trace_report_protocol)
+	}
 
-	batchSpanProcessor = newSpanProcessor(ctx)
+	batchSpanProcessor, err := createSpanProcessor(ctx, endpoint, reportProtocol)
+	if err != nil {
+		log.Fatalf("Failed to create the OpenTelemetry trace exporter, err: %v, endpoint: %s, report_protocol: %s", err, endpoint, reportProtocol)
+	}
 
 	var traceProvider *trace.TracerProvider
 	if batchSpanProcessor != nil {
@@ -96,6 +140,8 @@ func initOpenTelemetry() func() {
 
 	otel.SetTracerProvider(traceProvider)
 	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
+	log.Printf("OpenTelemetry initialized endpoint: %s, report_protocol: %s", endpoint, reportProtocol)
 
 	return func() {
 		cxt, cancel := context.WithTimeout(ctx, time.Second)
