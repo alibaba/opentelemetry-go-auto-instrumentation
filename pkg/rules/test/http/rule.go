815dde0b8bd27365a7f6a2f15e8f0438c846c18d
Liu Ziming <liuziming.lzm@alibaba-inc.com>
2024-08-28 09:22:46 +0800

    Feat/ci lint (#71)
    * fix ci lint

--- pkg/rules/test/http/rule.go
+++ pkg/rules/test/http/rule.go
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package http
 
 import "github.com/alibaba/opentelemetry-go-auto-instrumentation/api"
