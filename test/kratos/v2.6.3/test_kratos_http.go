5926570e46ae779e95cafe59fbe3493cfe4ff414
牧思 <liuziming.lzm@alibaba-inc.com>
2024-10-11 16:07:42 +0800

    fix test

--- test/kratos/v2.6.3/test_kratos_http.go
+++ test/kratos/v2.6.3/test_kratos_http.go
@@ -29,31 +29,31 @@ func main() {
 	}()
 	time.Sleep(5 * time.Second)
 	client := http.Client{}
-	client.Get("http://localhost:8080/helloworld/kratos")
+	client.Get("http://localhost:8000/helloworld/kratos")
 	fmt.Printf("Send http request to kratos")
 
 	verifier.WaitAndAssertTraces(func(stubs []tracetest.SpanStubs) {
-		protocolType := verifier.GetAttribute(stubs[0][1].Attributes, "kratos.protocol.type").AsString()
+		protocolType := verifier.GetAttribute(stubs[0][3].Attributes, "kratos.protocol.type").AsString()
 		if protocolType != "http" {
-			panic("protocol type should be grpc, actually got " + protocolType)
+			panic("protocol type should be http, actually got " + protocolType)
 		}
-		serviceName := verifier.GetAttribute(stubs[0][1].Attributes, "kratos.service.name").AsString()
+		serviceName := verifier.GetAttribute(stubs[0][3].Attributes, "kratos.service.name").AsString()
 		if serviceName != "opentelemetry-kratos-server" {
 			panic("service name should be opentelemetry-kratos-server, actually got " + serviceName)
 		}
-		serviceId := verifier.GetAttribute(stubs[0][1].Attributes, "kratos.service.id").AsString()
+		serviceId := verifier.GetAttribute(stubs[0][3].Attributes, "kratos.service.id").AsString()
 		if serviceId != "opentelemetry-id" {
 			panic("service id should be opentelemetry-id, actually got " + serviceId)
 		}
-		serviceVersion := verifier.GetAttribute(stubs[0][1].Attributes, "kratos.service.version").AsString()
+		serviceVersion := verifier.GetAttribute(stubs[0][3].Attributes, "kratos.service.version").AsString()
 		if serviceVersion != "v1" {
 			panic("service version should be v1, actually got " + serviceVersion)
 		}
-		serviceMetaAgent := verifier.GetAttribute(stubs[0][1].Attributes, "kratos.service.meta.agent").AsString()
+		serviceMetaAgent := verifier.GetAttribute(stubs[0][3].Attributes, "kratos.service.meta.agent").AsString()
 		if serviceMetaAgent != "opentelemetry-go" {
 			panic("service meta agent should be opentelemetry-go, actually got " + serviceMetaAgent)
 		}
-		serviceEndpoint := verifier.GetAttribute(stubs[0][1].Attributes, "kratos.service.endpoint").AsStringSlice()
+		serviceEndpoint := verifier.GetAttribute(stubs[0][3].Attributes, "kratos.service.endpoint").AsStringSlice()
 		if !strings.Contains(serviceEndpoint[0], ":9000") || !strings.Contains(serviceEndpoint[1], ":8000") {
 			panic("service endpoint should be grpc://30.221.144.142:9000 http://30.221.144.142:8000, actually got " + fmt.Sprintf("%v", serviceEndpoint))
 		}
