082737429f2a86afae18e34d9ff6d779c0ce7797
牧思 <liuziming.lzm@alibaba-inc.com>
2024-10-20 20:43:10 +0800

    fix http metrics test

--- test/nethttp/test_http_metrics.go
+++ test/nethttp/test_http_metrics.go
@@ -54,14 +54,15 @@ func main() {
 		verifier.VerifyHttpServerMetricsAttributes(point.DataPoints[0].Attributes.ToSlice(), "GET", "/a", "", "http", "1.1", "http", 200)
 	})
 
-	//verifier.WaitAndAssertMetrics("http.client.request.duration", func(mrs metricdata.ResourceMetrics) {
-	//	if len(mrs.ScopeMetrics) <= 0 {
-	//		panic("No http.client.request.duration metrics received!")
-	//	}
-	//	point := mrs.ScopeMetrics[0].Metrics[0].Data.(metricdata.Histogram[float64])
-	//	if point.DataPoints[0].Count != 1 {
-	//		panic("http.client.request.duration metrics count is not 1")
-	//	}
-	//	verifier.VerifyHttpClientMetricsAttributes(point.DataPoints[0].Attributes.ToSlice(), "GET", "127.0.0.1:"+strconv.Itoa(port), "", "http", "1.1", port, 200)
-	//})
+	verifier.WaitAndAssertMetrics("http.client.request.duration", func(mrs metricdata.ResourceMetrics) {
+		println("----------------------client------------------------")
+		if len(mrs.ScopeMetrics) <= 0 {
+			panic("No http.client.request.duration metrics received!")
+		}
+		point := mrs.ScopeMetrics[0].Metrics[0].Data.(metricdata.Histogram[float64])
+		if point.DataPoints[0].Count != 1 {
+			panic("http.client.request.duration metrics count is not 1")
+		}
+		verifier.VerifyHttpClientMetricsAttributes(point.DataPoints[0].Attributes.ToSlice(), "GET", "127.0.0.1:"+strconv.Itoa(port), "", "http", "1.1", port, 200)
+	})
 }
