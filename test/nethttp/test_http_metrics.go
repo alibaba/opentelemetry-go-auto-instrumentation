7772e4495b3b6a76d7cc43ca6b7fab9a64d690ac
Liu Ziming <liuziming.lzm@alibaba-inc.com>
2024-10-21 09:35:02 +0800

    add daily check for project (#140)
    * add daily check for project

--- test/nethttp/test_http_metrics.go
+++ test/nethttp/test_http_metrics.go
@@ -42,26 +42,27 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	time.Sleep(1 * time.Second)
-	verifier.WaitAndAssertMetrics("http.server.request.duration", func(mrs metricdata.ResourceMetrics) {
-		if len(mrs.ScopeMetrics) <= 0 {
-			panic("No http.server.request.duration metrics received!")
-		}
-		point := mrs.ScopeMetrics[0].Metrics[0].Data.(metricdata.Histogram[float64])
-		if point.DataPoints[0].Count != 1 {
-			panic("http.server.request.duration metrics count is not 1")
-		}
-		verifier.VerifyHttpServerMetricsAttributes(point.DataPoints[0].Attributes.ToSlice(), "GET", "/a", "", "http", "1.1", "http", 200)
-	})
-
-	verifier.WaitAndAssertMetrics("http.client.request.duration", func(mrs metricdata.ResourceMetrics) {
-		if len(mrs.ScopeMetrics) <= 0 {
-			panic("No http.server.request.duration metrics received!")
-		}
-		point := mrs.ScopeMetrics[0].Metrics[0].Data.(metricdata.Histogram[float64])
-		if point.DataPoints[0].Count != 1 {
-			panic("http.client.request.duration metrics count is not 1")
-		}
-		verifier.VerifyHttpClientMetricsAttributes(point.DataPoints[0].Attributes.ToSlice(), "GET", "127.0.0.1:"+strconv.Itoa(port), "", "http", "1.1", port, 200)
+	time.Sleep(3 * time.Second)
+	verifier.WaitAndAssertMetrics(map[string]func(metricdata.ResourceMetrics){
+		"http.server.request.duration": func(mrs metricdata.ResourceMetrics) {
+			if len(mrs.ScopeMetrics) <= 0 {
+				panic("No http.server.request.duration metrics received!")
+			}
+			point := mrs.ScopeMetrics[0].Metrics[0].Data.(metricdata.Histogram[float64])
+			if point.DataPoints[0].Count != 1 {
+				panic("http.server.request.duration metrics count is not 1")
+			}
+			verifier.VerifyHttpServerMetricsAttributes(point.DataPoints[0].Attributes.ToSlice(), "GET", "/a", "", "http", "1.1", "http", 200)
+		},
+		"http.client.request.duration": func(mrs metricdata.ResourceMetrics) {
+			if len(mrs.ScopeMetrics) <= 0 {
+				panic("No http.client.request.duration metrics received!")
+			}
+			point := mrs.ScopeMetrics[0].Metrics[0].Data.(metricdata.Histogram[float64])
+			if point.DataPoints[0].Count != 1 {
+				panic("http.client.request.duration metrics count is not 1")
+			}
+			verifier.VerifyHttpClientMetricsAttributes(point.DataPoints[0].Attributes.ToSlice(), "GET", "127.0.0.1:"+strconv.Itoa(port), "", "http", "1.1", port, 200)
+		},
 	})
 }
