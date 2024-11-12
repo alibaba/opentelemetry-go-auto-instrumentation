a571cf6c5345986aaee0ff41196732b131ac4b26
oliver  zhang <oliver_zhangjian@163.com>
2024-11-12 09:38:33 +0800

    add apache license header

--- pkg/inst-api/instrumenter/span_suppression_strategy.go
+++ pkg/inst-api/instrumenter/span_suppression_strategy.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2024 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package instrumenter
 
 import (
@@ -25,7 +39,6 @@ func (t *SemConvStrategy) create(spanKeys []attribute.Key) SpanSuppressor {
 	return bySpanKeySuppressor
 }
 
-
 type NoneStrategy struct{}
 
 func (n *NoneStrategy) create(spanKeys []attribute.Key) SpanSuppressor {
@@ -44,11 +57,9 @@ func (s *SpanKindStrategy) create(spanKeys []attribute.Key) SpanSuppressor {
 	return spanKindSuppressor
 }
 
-
 type SpanKindSuppressor struct {
 	delegates map[trace.SpanKind]SpanSuppressor
 }
-
 
 func getSpanSuppressionStrategyFromEnv() SpanSuppressorStrategy {
 	suppressionStrategy := os.Getenv("OTEL_INSTRUMENTATION_EXPERIMENTAL_SPAN_SUPPRESSION_STRATEGY")
