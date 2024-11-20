b4ed4d979d0bb399a5ea7c5e0a06aec89f89b575
牧思 <liuziming.lzm@alibaba-inc.com>
2024-11-20 16:18:08 +0800

    try to add go.opentelemetry.io/otel/sdk dependency

--- tool/preprocess/preprocess.go
+++ tool/preprocess/preprocess.go
@@ -78,7 +78,7 @@ var fixedDeps = []struct {
 	// otel sdk
 	{"go.opentelemetry.io/otel",
 		"v1.31.0", true, false},
-	{"go.opentelemetry.io/otel/trace",
+	{"go.opentelemetry.io/otel/sdk",
 		"v1.31.0", true, false},
 	{"go.opentelemetry.io/otel/exporters/otlp/otlptrace",
 		"v1.31.0", true, false},
