efe25ab9a38b550e8d987a3b5f571e304eaac0de
Martin Yonatan Pasaribu <107392127+martinyonatann@users.noreply.github.com>
2025-01-20 20:54:13 +0700

    Add Echo Metrics Test (#293)
    * echo metrics test

---
+++ test/echo/v4.0.0/test_echo_metrics.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2024 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/alibaba/opentelemetry-go-auto-instrumentation/test/verifier"
+	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+)
+
+func setup() {
+	engine := echo.New()
+	engine.Use(middleware.Logger())
+	engine.GET("/test", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"code": 1,
+			"msg":  c.Path(),
+		})
+	})
+	// Start server
+	engine.Logger.Fatal(engine.Start(":8080"))
+}
+
+func main() {
+	go setup()
+	time.Sleep(5 * time.Second)
+	client := &http.Client{}
+	resp, err := client.Get("http://127.0.0.1:8080/test")
+	defer resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(3 * time.Second)
+	verifier.WaitAndAssertMetrics(map[string]func(metricdata.ResourceMetrics){
+		"http.server.request.duration": func(mrs metricdata.ResourceMetrics) {
+			if len(mrs.ScopeMetrics) <= 0 {
+				panic("No http.server.request.duration metrics received!")
+			}
+			point := mrs.ScopeMetrics[0].Metrics[0].Data.(metricdata.Histogram[float64])
+			if point.DataPoints[0].Count <= 0 {
+				panic("http.server.request.duration metrics count is not positive, actually " + strconv.Itoa(int(point.DataPoints[0].Count)))
+			}
+			verifier.VerifyHttpServerMetricsAttributes(point.DataPoints[0].Attributes.ToSlice(), "GET", "/test", "", "http", "1.1", "http", 200)
+		},
+	})
+}
