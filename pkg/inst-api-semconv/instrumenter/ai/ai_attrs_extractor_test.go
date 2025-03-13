5f8fe3aa84b80cc2198f346b6f6504e068676087
zjlOffice <zjlOffice@limpiddream.com>
2025-03-13 15:36:46 +0800

    调整test

--- pkg/inst-api-semconv/instrumenter/ai/ai_attrs_extractor_test.go
+++ pkg/inst-api-semconv/instrumenter/ai/ai_attrs_extractor_test.go
@@ -110,34 +110,34 @@ func TestAILLMAttrsExtractorStart(t *testing.T) {
 	if len(attrs) == 0 {
 		t.Fatal("attrs is empty")
 	}
-	assert.Equal(t, "gen_ai.operation.name", attrs[0].Key) //semconv.GenAIOperationNameKey
+	assert.Equal(t, attribute.Key("gen_ai.operation.name"), attrs[0].Key) //semconv.GenAIOperationNameKey
 	assert.Equal(t, "llm", attrs[0].Value.AsString())
-	assert.Equal(t, "gen_ai.system", attrs[1].Key) //semconv.GenAISystemKey
+	assert.Equal(t, attribute.Key("gen_ai.system"), attrs[1].Key) //semconv.GenAISystemKey
 	assert.Equal(t, "langchain", attrs[1].Value.AsString())
 
-	assert.Equal(t, "gen_ai.request.model", attrs[2].Key) //semconv.GenAIRequestModelKey
+	assert.Equal(t, attribute.Key("gen_ai.request.model"), attrs[2].Key) //semconv.GenAIRequestModelKey
 	assert.Equal(t, "deepseek:17b", attrs[2].Value.AsString())
-	assert.Equal(t, "gen_ai.request.encoding_formats", attrs[3].Key) //semconv.GenAIRequestEncodingFormatsKey
+	assert.Equal(t, attribute.Key("gen_ai.request.encoding_formats"), attrs[3].Key) //semconv.GenAIRequestEncodingFormatsKey
 	assert.Equal(t, []string{"string"}, attrs[3].Value.AsStringSlice())
-	assert.Equal(t, "gen_ai.request.max_tokens", attrs[4].Key) //semconv.GenAIRequestMaxTokensKey
+	assert.Equal(t, attribute.Key("gen_ai.request.max_tokens"), attrs[4].Key) //semconv.GenAIRequestMaxTokensKey
 	assert.Equal(t, int64(10), attrs[4].Value.AsInt64())
-	assert.Equal(t, "gen_ai.request.frequency_penalty", attrs[5].Key) //semconv.GenAIRequestFrequencyPenaltyKey
+	assert.Equal(t, attribute.Key("gen_ai.request.frequency_penalty"), attrs[5].Key) //semconv.GenAIRequestFrequencyPenaltyKey
 	assert.Equal(t, 1.0, attrs[5].Value.AsFloat64())
-	assert.Equal(t, "gen_ai.request.presence_penalty", attrs[6].Key) //semconv.GenAIRequestPresencePenaltyKey
+	assert.Equal(t, attribute.Key("gen_ai.request.presence_penalty"), attrs[6].Key) //semconv.GenAIRequestPresencePenaltyKey
 	assert.Equal(t, 1.0, attrs[6].Value.AsFloat64())
-	assert.Equal(t, "gen_ai.request.stop_sequences", attrs[7].Key) //semconv.GenAIRequestStopSequencesKey
+	assert.Equal(t, attribute.Key("gen_ai.request.stop_sequences"), attrs[7].Key) //semconv.GenAIRequestStopSequencesKey
 	assert.Equal(t, []string{"stop"}, attrs[7].Value.AsStringSlice())
-	assert.Equal(t, "gen_ai.request.temperature", attrs[8].Key) //semconv.GenAIRequestTemperatureKey
+	assert.Equal(t, attribute.Key("gen_ai.request.temperature"), attrs[8].Key) //semconv.GenAIRequestTemperatureKey
 	assert.Equal(t, 1.0, attrs[8].Value.AsFloat64())
-	assert.Equal(t, "gen_ai.request.top_k", attrs[9].Key) //semconv.GenAIRequestTopKKey
+	assert.Equal(t, attribute.Key("gen_ai.request.top_k"), attrs[9].Key) //semconv.GenAIRequestTopKKey
 	assert.Equal(t, 1.0, attrs[9].Value.AsFloat64())
-	assert.Equal(t, "gen_ai.request.top_p", attrs[10].Key) //semconv.GenAIRequestTopPKey
+	assert.Equal(t, attribute.Key("gen_ai.request.top_p"), attrs[10].Key) //semconv.GenAIRequestTopPKey
 	assert.Equal(t, 1.0, attrs[10].Value.AsFloat64())
-	assert.Equal(t, "gen_ai.usage.input_tokens", attrs[11].Key) //semconv.GenAIUsageInputTokensKey
+	assert.Equal(t, attribute.Key("gen_ai.usage.input_tokens"), attrs[11].Key) //semconv.GenAIUsageInputTokensKey
 	assert.Equal(t, int64(10), attrs[11].Value.AsInt64())
 	assert.Equal(t, semconv.ServerAddressKey, attrs[12].Key)
 	assert.Equal(t, "127.0.0.1:1234", attrs[12].Value.AsString())
-	assert.Equal(t, "gen_ai.request.seed", attrs[13].Key) //semconv.GenAIRequestSeedKey
+	assert.Equal(t, attribute.Key("gen_ai.request.seed"), attrs[13].Key) //semconv.GenAIRequestSeedKey
 	assert.Equal(t, int64(100), attrs[13].Value.AsInt64())
 }
 func TestAILLMAttrsExtractorEnd(t *testing.T) {
@@ -148,14 +148,14 @@ func TestAILLMAttrsExtractorEnd(t *testing.T) {
 	attrs := make([]attribute.KeyValue, 0)
 	parentContext := context.Background()
 	attrs, _ = LLMExtractor.OnEnd(attrs, parentContext, testRequest{Operation: "llm", System: "langchain"}, testResponse{}, nil)
-	assert.Equal(t, "gen_ai.response.finish_reasons", attrs[0].Key) //semconv.GenAIResponseFinishReasonsKey
+	assert.Equal(t, attribute.Key("gen_ai.response.finish_reasons"), attrs[0].Key) //semconv.GenAIResponseFinishReasonsKey
 	assert.Equal(t, []string{"stop"}, attrs[0].Value.AsStringSlice())
-	assert.Equal(t, "gen_ai.response.id", attrs[1].Key) //semconv.GenAIResponseIDKey
+	assert.Equal(t, attribute.Key("gen_ai.response.id"), attrs[1].Key) //semconv.GenAIResponseIDKey
 	assert.Equal(t, "chatcmpl-123", attrs[1].Value.AsString())
-	assert.Equal(t, "gen_ai.response.model", attrs[2].Key) //semconv.GenAIResponseModelKey
+	assert.Equal(t, attribute.Key("gen_ai.response.model"), attrs[2].Key) //semconv.GenAIResponseModelKey
 	assert.Equal(t, "deepseek:17b", attrs[2].Value.AsString())
-	assert.Equal(t, "gen_ai.usage.output_tokens", attrs[3].Key) //semconv.GenAIUsageOutputTokensKey
+	assert.Equal(t, attribute.Key("gen_ai.usage.output_tokens"), attrs[3].Key) //semconv.GenAIUsageOutputTokensKey
 	assert.Equal(t, int64(10), attrs[3].Value.AsInt64())
-	assert.Equal(t, "gen_ai.response.id", attrs[4].Key) //semconv.GenAIResponseIDKey
+	assert.Equal(t, attribute.Key("gen_ai.response.id"), attrs[4].Key) //semconv.GenAIResponseIDKey
 	assert.Equal(t, "chatcmpl-123", attrs[4].Value.AsString())
 }
