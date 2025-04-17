26eb06d4f36c4a2d6d3884571c25059e9ff6e0a6
Huo Jiabin <huojiabin0924@gmail.com>
2025-04-17 13:17:57 +0800

    feat: add license header & add regular expression & delete unexpected lines generated

--- test/rocketmq/v2.1.2/base.go
+++ test/rocketmq/v2.1.2/base.go
@@ -1,21 +1,34 @@
+// Copyright (c) 2025 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"context"
 	"fmt"
 	"github.com/alibaba/opentelemetry-go-auto-instrumentation/test/verifier"
+	"github.com/apache/rocketmq-client-go/v2"
 	"github.com/apache/rocketmq-client-go/v2/admin"
 	"github.com/apache/rocketmq-client-go/v2/consumer"
 	"github.com/apache/rocketmq-client-go/v2/primitive"
+	"github.com/apache/rocketmq-client-go/v2/producer"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	"go.opentelemetry.io/otel/trace"
 	"log"
 	"os"
 	"time"
-
-	"github.com/apache/rocketmq-client-go/v2"
-	"github.com/apache/rocketmq-client-go/v2/producer"
 )
 
 const (
--- test/rocketmq/v2.1.2/test_mq_consumer.go
+++ test/rocketmq/v2.1.2/test_mq_consumer.go
@@ -1,21 +1,25 @@
-// Copyright (c) 2024 Alibaba Group Holding Ltd.
+// Copyright (c) 2025 Alibaba Group Holding Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package main
 
-//line <generated>:1
 import (
+	"context"
+	"fmt"
 	"github.com/alibaba/opentelemetry-go-auto-instrumentation/test/verifier"
+	"github.com/apache/rocketmq-client-go/v2/consumer"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
 	_ "go.opentelemetry.io/otel"
 	_ "go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	_ "go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
@@ -24,17 +28,8 @@ import (
 	_ "go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	_ "go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
-)
-
-//line test_mq_consumer.go:16:1
-import (
-	"context"
-	"fmt"
 	"log"
 	"time"
-
-	"github.com/apache/rocketmq-client-go/v2/consumer"
-	"github.com/apache/rocketmq-client-go/v2/primitive"
 )
 
 // 自定义不同的消费组常量
