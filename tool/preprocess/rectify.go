420eebd542af7d3ba155f84dac578120f280fe52
Yi Yang <qingfeng.yy@alibaba-inc.com>
2025-06-27 17:01:52 +0800

    pinning more

--- tool/preprocess/rectify.go
+++ tool/preprocess/rectify.go
@@ -34,6 +34,8 @@ var otelDeps = map[string]string{
 	"go.opentelemetry.io/otel":                                          "v1.35.0",
 	"go.opentelemetry.io/otel/sdk":                                      "v1.35.0",
 	"go.opentelemetry.io/otel/trace":                                    "v1.35.0",
+	"go.opentelemetry.io/otel/metric":                                   "v1.35.0",
+	"go.opentelemetry.io/otel/sdk/metric":                               "v1.35.0",
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace":                 "v1.35.0",
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc":   "v1.35.0",
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp":   "v1.35.0",
