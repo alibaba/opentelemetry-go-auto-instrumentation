1180ae749975d20fb3a369e995026f19643f338f
mario0508 <33172201+mario0508@users.noreply.github.com>
2025-07-08 18:46:18 +0530

    Update rpc_metrics.go to include gRPC status code in metrics filter

--- pkg/inst-api-semconv/instrumenter/rpc/rpc_metrics.go
+++ pkg/inst-api-semconv/instrumenter/rpc/rpc_metrics.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      rpc://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -32,12 +32,12 @@ const rpc_server_request_duration = "rpc.server.duration"
 const rpc_client_request_duration = "rpc.client.duration"
 
 type RpcServerMetric struct {
-	key                   attribute.Key
+	key                    attribute.Key
 	serverRequestDuration metric.Float64Histogram
 }
 
 type RpcClientMetric struct {
-	key                   attribute.Key
+	key                    attribute.Key
 	clientRequestDuration metric.Float64Histogram
 }
 
@@ -48,12 +48,13 @@ var rpcMetricsConv = map[attribute.Key]bool{
 	semconv.RPCMethodKey:     true,
 	semconv.RPCServiceKey:    true,
 	semconv.ServerAddressKey: true,
+	semconv.RPCGRPCStatusCodeKey: true,
 }
 
 var globalMeter metric.Meter
 
 // InitRpcMetrics so we need to make sure the otel_setup is executed before all the init() function
-// related to issue rpcs://github.com/alibaba/opentelemetry-go-auto-instrumentation/issues/48
+// related to issue https://github.com/alibaba/opentelemetry-go-auto-instrumentation/issues/48
 func InitRpcMetrics(m metric.Meter) {
 	mu.Lock()
 	defer mu.Unlock()
@@ -84,7 +85,7 @@ func newRpcServerRequestDurationMeasures(meter metric.Meter) (metric.Float64Hist
 	if err == nil {
 		return d, nil
 	} else {
-		return d, errors.New(fmt.Sprintf("failed to create rpc.server.request.duratio histogram, %v", err))
+		return d, errors.New(fmt.Sprintf("failed to create rpc.server.request.duration histogram, %v", err))
 	}
 }
 
