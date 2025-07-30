d950b4b6841787248896d582e15daa960e153ab2
_Rinai <rinai@g-rinai.cn>
2025-07-30 12:26:49 +0800

    chore: fix for test

--- test/hertz/v0.9.0/test_hertz_with_exception.go
+++ test/hertz/v0.9.0/test_hertz_with_exception.go
@@ -29,7 +29,7 @@ func main() {
 	GetException()
 	verifier.WaitAndAssertTraces(func(stubs []tracetest.SpanStubs) {
 		verifier.VerifyHttpClientAttributes(stubs[0][0], "GET", "GET", "http://127.0.0.1:8888/exception", "http", "", "tcp", "ipv4", "", "127.0.0.1:8888", 500, 0, 8888)
-		verifier.VerifyHttpServerAttributes(stubs[0][1], "GET /exception", "GET", "http", "tcp", "ipv4", "", "127.0.0.1:8888", "Host", "http", "/exception", "", "/exception", 500)
+		verifier.VerifyHttpServerAttributes(stubs[0][1], "/exception", "GET", "http", "tcp", "ipv4", "", "127.0.0.1:8888", "Host", "http", "/exception", "", "/exception", 500)
 		fmt.Printf("%v %v\n", stubs[0][0].Status.Code, stubs[0][1].Status.Code)
 		if stubs[0][0].Status.Code != codes.Error {
 			panic("span should be error state")
